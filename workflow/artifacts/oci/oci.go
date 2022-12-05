e86bc18502456e921812d6c848c0e392cd1c4ed6
Chad Malfait <chad.malfait@oracle.com>
2022-12-04 20:44:16 -0700

    feat: cleanup oci.go
    Signed-off-by: Chad Malfait <chad.malfait@oracle.com>

--- workflow/artifacts/oci/oci.go
+++ workflow/artifacts/oci/oci.go
@@ -2,17 +2,14 @@ package oci
 
 import (
 	"context"
-	"crypto/sha256"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"math/rand"
-	"os"
-	"path"
+	"path/filepath"
 	"strings"
 
 	"time"
 
+	"github.com/oracle/oci-go-sdk/v65/objectstorage/transfer"
 	"github.com/oracle/oci-go-sdk/v65/objectstorage"
 
 	ocicommon "github.com/oracle/oci-go-sdk/v65/common"
@@ -25,12 +22,6 @@ import (
 	"github.com/argoproj/argo-workflows/v3/workflow/artifacts/common"
 )
 
-//const tenancyOCID string = ""
-//const userOCID string = ""
-//const region string = "us-phoenix-1"
-//const fingerprint string = ""
-
-// const compartmentOCID string = ""
 type ArtifactDriver struct {
 	Provider        string
 	CompartmentOCID string
@@ -50,39 +41,26 @@ var (
 )
 
 func (ociDriver *ArtifactDriver) initializeConfigurationProvider() (context.Context, objectstorage.ObjectStorageClient) {
-	log.Infof("DEBUG initializeConfigurationProvider")
 	var configurationProvider ocicommon.ConfigurationProvider
 
-	// We accept 3 types of auth. 
-	// 1. instance_principal, 2. k8s_secret, 3. raw
-	// default to instance principal
+	// We accept 3 types of auth.  1. instance_principal, 2. k8s_secret, 3. raw
+	// We default to instance principal
 	switch ociDriver.Provider {
 	case "instance_principal":
-		fmt.Println("DEBUG: Instance Principal: ", ociDriver.Provider)
-		log.Infof("DEBUG: Instance Principal: %v", ociDriver.Provider)
-		configurationProvider = initializeConfigurationProvider()
+		configurationProvider = initializeInstancePrincipalProvider() 
 	case "k8s_secret":
-		fmt.Println("DEBUG: Secrets: ", ociDriver.SecretKey)
-		log.Infof("DEBUG: Secrets: %v", ociDriver.SecretKey)
-		fmt.Println("DEBUG: Access: ", ociDriver.AccessKey)
-		log.Infof("DEBUG: Access: %v", ociDriver.AccessKey)
+		// Trim whitespace
 		access := strings.TrimSpace(ociDriver.AccessKey)
-		fmt.Println("DEBUG: AccessTrim: ", access)
-		log.Infof("DEBUG: AccessTrim: %v", access)
 		configurationProvider = ocicommon.NewRawConfigurationProvider(ociDriver.TenancyOCID, ociDriver.UserOCID, ociDriver.Region, access, ociDriver.SecretKey, nil)
 	case "raw":
-		fmt.Println("DEBUG: raw: ", ociDriver.SecretKey)
-		log.Infof("DEBUG: raw: %v", ociDriver.SecretKey)
 		configurationProvider = ocicommon.NewRawConfigurationProvider(ociDriver.TenancyOCID, ociDriver.UserOCID, ociDriver.Region, ociDriver.Fingerprint, ociDriver.PrivateKey, nil)
 	default:
-		fmt.Println("DEBUG: Provider not set, default to instance principal")
-		log.Infof("DEBUG: Provider not set, default to instance principal")
-		configurationProvider = initializeConfigurationProvider()
+		configurationProvider = initializeInstancePrincipalProvider() 
 	}
 
 	storageClient, err := objectstorage.NewObjectStorageClientWithConfigurationProvider(configurationProvider)
 	if err != nil {
-		log.Errorf("Object client error %s", err)
+		log.Errorf("ObjectStorageClient error %s", err)
 	}
 
 	ctx := context.Background()
@@ -90,19 +68,15 @@ func (ociDriver *ArtifactDriver) initializeConfigurationProvider() (context.Cont
 }
 
 // instance principal authentication
-func initializeConfigurationProvider() ocicommon.ConfigurationProvider {
-	fmt.Println("DEBUG: INSTANCE PRINCIPAL configuration provider")
-	log.Infof("DEBUG: INSTANCE Principal configurationProvider")
+func initializeInstancePrincipalProvider() ocicommon.ConfigurationProvider {
 	configurationProvider, err := ociauth.InstancePrincipalConfigurationProvider()
 	if err != nil {
-		fmt.Println("error in instance principal config provider ", err)
+		log.Errorf("Error in instance principal config provider %s", err)
 	}
 	return configurationProvider
 }
 
 func (ociDriver *ArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) error {
-	log.Infof("DEBUG Load")
-
 	err := waitutil.Backoff(defaultRetry,
 		func() (bool, error) {
 			log.Infof("OCI Load path: %s, key %s", path, inputArtifact.OCI.Key)
@@ -113,25 +87,20 @@ func (ociDriver *ArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string)
 }
 
 func (ociDriver *ArtifactDriver) OpenStream(artifact *wfv1.Artifact) (io.ReadCloser, error) {
-	log.Infof("DEBUG OpenStream")
+	log.Info("DEBUG: OpenStream")
 	return common.LoadToStream(artifact, ociDriver)
 }
 
 func (ociDriver *ArtifactDriver) Save(path string, outputArtifact *wfv1.Artifact) error {
-	log.Infof("DEBUG Save")
+	log.Info("DEBUG: Save")
 	err := waitutil.Backoff(defaultRetry,
 		func() (bool, error) {
-			log.Infof("OCI Save path: #{path}, key: #{outputArtifact.OCI.Key")
+			objectName := filepath.Clean(outputArtifact.OCI.Key)
 			ctx, storageClient := ociDriver.initializeConfigurationProvider()
-
 			namespace := getNamespace(ctx, storageClient)
 			bucketName := outputArtifact.OCI.OCIBucket.Bucket
-			objectName := outputArtifact.OCI.Key
 
-			fmt.Println("SAVING" + namespace + "/" + bucketName + "/" + objectName)
-			log.Infof("SAVING" + namespace + "/" + bucketName + "/" + objectName)
-
-			err := putObject(ctx, storageClient, namespace, bucketName, objectName, nil)
+			err := putObject(ctx, storageClient, namespace, path, bucketName, objectName, nil)
 			if err != nil {
 				log.Errorf("Error putting object: %v", err)
 			}
@@ -139,6 +108,46 @@ func (ociDriver *ArtifactDriver) Save(path string, outputArtifact *wfv1.Artifact
 			return true, nil
 		})
 	return err
+}
+
+// upload a local object or dir to OCI storage
+func putObject(ctx context.Context, storageClient objectstorage.ObjectStorageClient, namespace, path, bucketName, objectName string, metadata map[string]string) error {
+	uploadManager := transfer.NewUploadManager()
+	req := transfer.UploadFileRequest{
+		UploadRequest: transfer.UploadRequest{
+			NamespaceName:                       ocicommon.String(namespace),
+			BucketName:                          ocicommon.String(bucketName),
+			ObjectName:                          ocicommon.String(objectName),
+			PartSize:                            ocicommon.Int64(128 * 1024 * 1024),
+			CallBack:                            callBack,
+			ObjectStorageClient:                 &storageClient,
+			EnableMultipartChecksumVerification: ocicommon.Bool(true),
+		},
+		FilePath: path,
+	}
+		
+	// if you want to overwrite default value, you can do it
+	// as: transfer.UploadRequest.AllowMultipartUploads = common.Bool(false) // default is true
+	// or: transfer.UploadRequest.AllowParrallelUploads = common.Bool(false) // default is true
+	resp, err := uploadManager.UploadFile(ctx, req)
+
+	if err != nil && resp.IsResumable() {
+		resp, err = uploadManager.ResumeUploadFile(ctx, *resp.MultipartUploadResponse.UploadID)
+		if err != nil {
+			fmt.Println(resp)
+		}
+	}
+	return err
+}
+
+func callBack(multiPartUploadPart transfer.MultiPartUploadPart) {
+	if nil == multiPartUploadPart.Err {
+		// Please refer this as the progress bar print content.
+		fmt.Printf("Part: %d / %d is uploaded.\n", multiPartUploadPart.PartNum, multiPartUploadPart.TotalParts)
+		fmt.Printf("One example of progress bar could be the above comment content.\n")
+		// Please refer following fmt to get each part opc-md5 res.
+		fmt.Printf("and this part opcMD5(64BasedEncoding) is: %s.\n", *multiPartUploadPart.OpcMD5 )
+	}
 }
 
 // log fatal error
@@ -158,63 +167,15 @@ func getNamespace(ctx context.Context, client objectstorage.ObjectStorageClient)
 	return *r.Value
 }
 
-// upload a local object or dir to OCI storage
-func putObject(ctx context.Context, c objectstorage.ObjectStorageClient, namespace, bucketname, objectname string, metadata map[string]string) error {
-	//var contentlen int64
-	contentlen := 1024 * 1000
-
-	filepath, filesize := WriteTempFileOfSize(int64(contentlen))
-	filename := path.Base(filepath)
-
-	defer func() {
-		os.Remove(filename)
-	}()
-
-	file, e := os.Open(filepath)
-	if e != nil {
-		log.Infof("Error opening file: %s", e.Error())
-	}
-	defer file.Close()
-	fatalIfError(e)
-
-	request := objectstorage.PutObjectRequest{
-		NamespaceName: &namespace,
-		BucketName:    &bucketname,
-		ObjectName:    &objectname,
-		ContentLength: &filesize,
-		PutObjectBody: file,
-		OpcMeta:       metadata,
-	}
-	_, err := c.PutObject(ctx, request)
-	fmt.Println("Put object ", objectname, " in bucket ", bucketname)
-	return err
-}
-
-func WriteTempFileOfSize(filesize int64) (fileName string, fileSize int64) {
-	hash := sha256.New()
-	f, _ := ioutil.TempFile("", "OCIGOSDKSampleFile")
-	ra := rand.New(rand.NewSource(time.Now().UnixNano()))
-	defer f.Close()
-	writer := io.MultiWriter(f, hash)
-	written, _ := io.CopyN(writer, ra, filesize)
-	fileName = f.Name()
-	fileSize = written
-	return
-}
-
 func (ociDriver *ArtifactDriver) Delete(artifact *wfv1.Artifact) error {
-	log.Infof("DEBUG Delete")
+	log.Info("DEBUG: Delete")
 	err := waitutil.Backoff(defaultRetry,
 		func() (bool, error) {
-			log.Infof("OCI Delete: #{outputArtifact.OCI.Key")
 			ctx, storageClient := ociDriver.initializeConfigurationProvider()
 
 			namespace := getNamespace(ctx, storageClient)
 			bucketName := artifact.OCI.OCIBucket.Bucket
 			objectName := artifact.OCI.Key
-
-			fmt.Println("DELETING: " + namespace + "/" + bucketName + "/" + objectName)
-			log.Infof("DELETING" + namespace + "/" + bucketName + "/" + objectName)
 
 			err := deleteObject(ctx, storageClient, namespace, bucketName, objectName)
 			if err != nil {
@@ -238,6 +199,7 @@ func deleteObject(ctx context.Context, c objectstorage.ObjectStorageClient, name
 }
 
 func (ociDriver *ArtifactDriver) ListObjects(artifact *wfv1.Artifact) ([]string, error) {
+	log.Info("DEBUG: ListObjects")
 	var files []string
 	err := waitutil.Backoff(defaultRetry,
 		func() (bool, error) {
@@ -247,6 +209,7 @@ func (ociDriver *ArtifactDriver) ListObjects(artifact *wfv1.Artifact) ([]string,
 }
 
 func (ociDriver *ArtifactDriver) IsDirectory(artifact *wfv1.Artifact) (bool, error) {
+	log.Info("DEBUG: IsDirectory")
 	err := waitutil.Backoff(defaultRetry,
 		func() (bool, error) {
 			return true, nil
@@ -255,5 +218,6 @@ func (ociDriver *ArtifactDriver) IsDirectory(artifact *wfv1.Artifact) (bool, err
 }
 
 func isTransientOCIErr() bool {
+	log.Info("DEBUG: isTransientOCIErr")
 	return true
 }
